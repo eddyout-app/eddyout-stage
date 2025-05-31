3430181fd70c16df67c7f5f62e9226f001bf88d2
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-05-31 13:53:41 -0600

    created expense model and updated crew model to assist

--- server/src/models/crew.ts
+++ server/src/models/crew.ts
@@ -1,54 +1,10 @@
-// import { DataTypes, type Sequelize, Model, Optional } from "sequelize";
-// //Create the items that are unique to crew
-// interface CrewAttributes {
-//   id: string;
-//   tripId: string;
-//   userId: string;
-// }
+import mongoose from "mongoose";
 
-// //extends the trip information associated with the crew member
-// interface CrewCreationAttributes extends Optional<CrewAttributes, "id"> {}
+const crewSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    tripId: { type: mongoose.Schema.Types.ObjectId, ref: "Trip", required: true },
+    role: { type: String }, // Optional
+});
 
-// export class Crew extends Model<CrewAttributes, CrewCreationAttributes> {
-//   public id!: string;
-//   public tripId!: string;
-//   public userId!: string;
-
-//   public readonly createdAt!: Date;
-//   public readonly updatedAt!: Date;
-// }
-
-// export function CrewFactory(sequelize: Sequelize): typeof Crew {
-//   Crew.init(
-//     {
-//       id: {
-//         type: DataTypes.UUID,
-//         defaultValue: DataTypes.UUIDV4,
-//         primaryKey: true,
-//       },
-//       tripId: {
-//         type: DataTypes.UUID,
-//         allowNull: false,
-//         references: {
-//           model: "trips",
-//           key: "id",
-//         },
-//         onDelete: "CASCADE",
-//         onUpdate: "CASCADE",
-//       },
-//       userId: {
-//         type: DataTypes.UUID,
-//         allowNull: false,
-//         references: {
-//           model: "users",
-//           key: "id",
-//         },
-//       },
-//     },
-//     {
-//       tableName: "crew",
-//       sequelize,
-//     }
-//   );
-//   return Crew;
-// }
+const Crew = mongoose.model("Crew", crewSchema);
+export default Crew;
